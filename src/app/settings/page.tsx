d1ded869c9e2bf4a7b3bbfbbfba8eb4194aead7d
tanukizzan <51444253+tanukizzan@users.noreply.github.com>
2025-05-02 20:20:10 +0900

    create components files

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center">
+      <span className="text-2xl font-bold text-gray-900 dark:text-white">
+        Settings
+      </span>
+    </div>
+  );
+}
---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+export default function Logo() {
+  return (
+    <div className="flex items-center">
+      <span className="text-2xl font-bold text-gray-900 dark:text-white">
+        fisea
+      </span>
+    </div>
+  );
+}
